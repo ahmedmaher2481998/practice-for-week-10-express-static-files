54c3cabc6a60545e04ba2b5cb982a2440d0f7de9
ahmedmaher2481998 <ahmedmaher2481998@yahoo.com>
2022-09-12 11:15:39 +0200

    done

--- server/app.js
+++ server/app.js
@@ -1,6 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 // Your code here
-
+// part 1
+// app.use(express.static(`${__dirname}/assets/scripts`));
+// part 2
+// app.use("/scripts", express.static(`assets/scripts`));
+// part 3
+app.use("/stylesheets", express.static(`${__dirname}/assets/css`));
 const port = 5000;
-app.listen(port, () => console.log('Server is listening on port', port));
+app.listen(port, () => console.log("Server is listening on port", port));
